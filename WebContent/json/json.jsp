fdff72408c2f2d526557c7726eba77f3952b6692
ay91911 <ay91911@gmail.com>
2020-06-19 18:19:53 +0900

    2020-06-19-v1

---
+++ WebContent/json/json.jsp
@@ -0,0 +1,34 @@
+<%
+	JSONObject obj = new JSONObject();
+	obj.put("name","sung");
+	obj.put("age",19);
+	obj.put("address","서울");
+	
+	JSONArray array = new JSONArray();
+	
+	JSONObject obj1 = new JSONObject();
+	obj1.put("no","1");
+	obj1.put("storeName","강남스토어1");
+	
+	JSONObject obj2 = new JSONObject();
+	obj2.put("no","2");
+	obj2.put("storeName","강남스토어2");
+	
+	JSONObject obj3 = new JSONObject();
+	obj3.put("no","3");
+	obj3.put("storeName","강남스토어3");
+	
+	array.add(obj1);
+	array.add(obj2);
+	array.add(obj3);
+	
+	obj.put("item",array);
+	out.print(obj.toJSONString());
+%>
+
+<%@page import="org.json.simple.JSONArray"%>
+<%@page import="org.json.simple.JSONObject"%>
+<%@ page language="java" contentType="application/json; charset=UTF-8"%>
+
+ 
+
--- src/kr/or/kpc/dao/NoticeDao.java
+++ src/kr/or/kpc/dao/NoticeDao.java
@@ -6,6 +6,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import kr.or.kpc.dto.NoticeDto;
 import kr.or.kpc.util.ConnLocator;
 
@@ -331,6 +334,120 @@ public class NoticeDao {
 		
 		
 		return max;
+	}
+	
+	public String selectJson(int start, int len){//웹은  ArrayList를 받지 못함으로 문자열로 반환
+		
+		
+		/*Json버전
+		 
+		 {
+		 "items":[
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			 {"num":1, "writer","작성자1","title":"제목1","regdate":"2020..."},
+			.....
+			
+			]
+			 		 
+		 }
+		 
+		 
+		 Xml버전
+		 <items>
+		 	<item>
+		 		<num></num>
+		 		<writer></writer>
+		 		<title></title>
+		 		<regdate></reagdate>
+		 	</item>
+		 	<item>
+		 		<num></num>
+		 		<writer></writer>
+		 		<title></title>
+		 		<regdate></reagdate>
+		 	</item>
+		 	<item>
+		 		<num></num>
+		 		<writer></writer>
+		 		<title></title>
+		 		<regdate></reagdate>
+		 	</item>
+		 	<item>
+		 		<num></num>
+		 		<writer></writer>
+		 		<title></title>
+		 		<regdate></reagdate>
+		 	</item>
+		 	.............
+		 </items>
+		 
+		
+		
+		  */
+		JSONObject obj = new JSONObject();
+		JSONArray array = new JSONArray();
+		
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = ConnLocator.getConnect();
+
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT n_num, n_writer, n_title, n_content, date_format(n_regdate, '%Y.%m.%d %h:%i') ");
+			sql.append("FROM notice ");
+			sql.append("ORDER BY n_num DESC ");
+			sql.append("LIMIT ?,? ");
+
+			pstmt = con.prepareStatement(sql.toString());
+
+			int index = 0;
+			pstmt.setInt(++index, start);
+			pstmt.setInt(++index, len);
+			
+
+			rs = pstmt.executeQuery();
+			JSONObject obj1 = null;
+			
+			while (rs.next()) {
+				index = 0;
+				int num = rs.getInt(++index);
+				String writer = rs.getString(++index);
+				String title = rs.getString(++index);
+				String content = rs.getString(++index);
+				String regdate = rs.getString(++index);
+				obj1 = new JSONObject();
+				obj1.put("num",String.valueOf(num));//json에선 숫자도 문자열로 반환하도록
+				obj1.put("writer",writer);
+				obj1.put("title",title);
+				obj1.put("regdate",regdate);
+				array.add(obj1);
+			}
+			obj.put("items",array);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return obj.toJSONString();
+		
 	}
 	
 }
